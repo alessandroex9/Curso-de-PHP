6b403d357d1a0ff09bdefa212ccb8511742a606a
Alessandro Martins dos Santos <alessandromartinsdosantos@gmail.com>
2024-06-23 06:31:12 -0300

    create exercicios

---
+++ exercicios/ex003/index.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Exempo PHP</title>
+</head>
+<body>
+    <h1>Exemplo PHP</h1>
+    <?php
+        $nome = "Alessandro Martins";
+        echo "<p>Olá, $nome</p>";
+    ?>
+
+    <?= "Alessandro Martins"?>
+</body>
+</html>
